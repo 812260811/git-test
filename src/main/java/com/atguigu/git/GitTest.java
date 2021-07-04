51f0bd295b89e160c82e0ab4b08b4184d12aeeda
RenAi <812260811@qq.com>
2021-07-04 14:57:47 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,6 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("老表干妹子");
-        System.out.println("老表干妹子");
+        System.out.println("老表干妹子a");
     }
 }
