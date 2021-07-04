c258abf0b33ba4248a5eed82d9971256fa8d0048
RenAi <812260811@qq.com>
2021-07-04 21:14:39 +0800

    Go to GitHub!

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,6 +9,6 @@ public class GitTest {
         System.out.println("push test");
         System.out.println("agains");
         System.out.println("gitee");
-        System.out.println("我在Gitee上进行操作！");
+        System.out.println("Go to Github!");
     }
 }
